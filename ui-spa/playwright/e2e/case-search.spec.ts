c32aa6fa95cd283d43a10dc5cd02f54b6b889a29
renjithabby <renjithabby@gmail.com>
2025-03-14 17:42:51 +0000

    updated teh test

--- ui-spa/playwright/e2e/case-search.spec.ts
+++ ui-spa/playwright/e2e/case-search.spec.ts
@@ -1,8 +1,30 @@
 import { test, expect } from "@playwright/test";
 
-test("Search results page", async ({ page }) => {
-  await page.goto("/search-results?urn=11AA2222233");
-
-  await expect(page.locator("h1")).toHaveText("Search for urn search");
-  await expect(page.locator("body")).toContainText("Thunderstruck1_pl");
+test.describe("Case Search", () => {
+  test("case search by operation name and area", async ({ page }) => {
+    await page.goto("/");
+    await page.waitForResponse(`https://mocked-out-api/api/areas`);
+    await page.getByRole("radio", { name: "Operation name" }).check();
+    const input = await page.locator('label:text("Operation name") + input');
+    await input.isVisible();
+    await input.fill("thunder");
+    const areaSelect = await page.getByTestId("search-operation-area");
+    const options = await areaSelect.locator("option").allTextContents();
+    await expect(options).toEqual([
+      "-- Please select --",
+      "SEOCID Int London and SE Div",
+      "Special Crime Division",
+      "Bedfordshire",
+      "Cambridgeshire",
+      "Cheshire",
+    ]);
+    areaSelect.selectOption("Cambridgeshire");
+    await page.locator('button:text("search")').click();
+    await expect(page).toHaveURL(
+      /\/search-results\?operation-name=thunder&area=4/,
+    );
+    await await expect(page.locator("h1")).toHaveText(
+      "Search for Operation name search",
+    );
+  });
 });
