f14dac85c5782f819ff6301c688199c3fd0344cb
Mark Jones <mark.idevelop@gmail.com>
2025-03-24 16:26:33 +0000

    Replaced references to egressmock and netappmock with a single mock definition

--- terraform/application/app-service-mock.tf
+++ terraform/application/app-service-mock.tf
@@ -1,15 +1,15 @@
-resource "azurerm_linux_web_app" "complex_cases_egressMock" {
+resource "azurerm_linux_web_app" "complex_cases_mock" {
   #checkov:skip=CKV_AZURE_13:nsure App Service Authentication is set on Azure App Service
   #checkov:skip=CKV_AZURE_88:Ensure that app services use Azure Files
   #checkov:skip=CKV_AZURE_16:Ensure that Register with Azure Active Directory is enabled on App Service
   #checkov:skip=CKV_AZURE_213:Ensure that App Service configures health check
   #checkov:skip=CKV_AZURE_71:Ensure that Managed identity provider is enabled for app services
   #checkov:skip=CKV_AZURE_17:Ensure the web app has 'Client Certificates (Incoming client certificates)' set
-  name                          = "${local.product_prefix}-egress-mock"
+  name                          = "${local.product_prefix}-mock"
   resource_group_name           = azurerm_resource_group.rg_complex_cases.name
   location                      = azurerm_resource_group.rg_complex_cases.location
-  service_plan_id               = azurerm_service_plan.asp_complex_cases_egressMock.id
-  virtual_network_subnet_id     = azurerm_subnet.sn_complex_cases_egressMock_subnet.id
+  service_plan_id               = azurerm_service_plan.asp_complex_cases_mock.id
+  virtual_network_subnet_id     = azurerm_subnet.sn_complex_cases_mock_subnet.id
   public_network_access_enabled = false
   https_only                    = true
 
@@ -18,8 +18,8 @@ resource "azurerm_linux_web_app" "complex_cases_egressMock" {
     "WEBSITE_CONTENTOVERVNET"                    = "1"
     "WEBSITE_DNS_SERVER"                         = var.dns_server
     "WEBSITE_DNS_ALT_SERVER"                     = var.dns_alt_server
-    "WEBSITE_CONTENTAZUREFILECONNECTIONSTRING"   = azurerm_storage_account.sacpsccui.primary_connection_string
-    "WEBSITE_CONTENTSHARE"                       = azapi_resource.sacpsccui_egressMock_file_share.name
+    "WEBSITE_CONTENTAZUREFILECONNECTIONSTRING"   = azurerm_storage_account.sacpsccapi.primary_connection_string
+    "WEBSITE_CONTENTSHARE"                       = azapi_resource.sacpsccapi_mock_file_share.name
     "ApplicationInsightsAgent_EXTENSION_VERSION" = "~3"
     "XDT_MicrosoftApplicationInsights_Mode"      = "Recommended"
   }
@@ -43,7 +43,7 @@ resource "azurerm_linux_web_app" "complex_cases_egressMock" {
       tenant_auth_endpoint = "https://sts.windows.net/${data.azurerm_client_config.current.tenant_id}/v2.0"
       #checkov:skip=CKV_SECRET_6:Base64 High Entropy String - Misunderstanding of setting "MICROSOFT_PROVIDER_AUTHENTICATION_SECRET"
       client_secret_setting_name = "MICROSOFT_PROVIDER_AUTHENTICATION_SECRET"
-      client_id                  = azuread_application.complex_cases_egressMock.client_id
+      client_id                  = azuread_application.complex_cases_mock.client_id
     }
 
     login {
@@ -74,9 +74,9 @@ resource "azurerm_linux_web_app" "complex_cases_egressMock" {
   }
 }
 
-resource "azuread_application" "complex_cases_egressMock" {
-  display_name            = "${local.product_prefix}-egressMock-appreg"
-  identifier_uris         = ["https://CPSGOVUK.onmicrosoft.com/${local.product_prefix}-egressMock"]
+resource "azuread_application" "complex_cases_mock" {
+  display_name            = "${local.product_prefix}-mock-appreg"
+  identifier_uris         = ["https://CPSGOVUK.onmicrosoft.com/${local.product_prefix}-mock"]
   prevent_duplicate_names = true
   owners                  = [data.azuread_service_principal.terraform_service_principal.object_id]
 
@@ -90,8 +90,8 @@ resource "azuread_application" "complex_cases_egressMock" {
   }
 }
 
-resource "azurerm_private_endpoint" "complex_cases_egressMock_pe" {
-  name                = "${azurerm_linux_web_app.complex_cases_egressMock.name}-pe"
+resource "azurerm_private_endpoint" "complex_cases_mock_pe" {
+  name                = "${azurerm_linux_web_app.complex_cases_mock.name}-pe"
   resource_group_name = azurerm_resource_group.rg_complex_cases.name
   location            = azurerm_resource_group.rg_complex_cases.location
   subnet_id           = azurerm_subnet.sn_complex_cases_endpoints_subnet.id
@@ -103,8 +103,8 @@ resource "azurerm_private_endpoint" "complex_cases_egressMock_pe" {
   }
 
   private_service_connection {
-    name                           = "${azurerm_linux_web_app.complex_cases_egressMock.name}-psc"
-    private_connection_resource_id = azurerm_linux_web_app.complex_cases_egressMock.id
+    name                           = "${azurerm_linux_web_app.complex_cases_mock.name}-psc"
+    private_connection_resource_id = azurerm_linux_web_app.complex_cases_mock.id
     is_manual_connection           = false
     subresource_names              = ["sites"]
   }
