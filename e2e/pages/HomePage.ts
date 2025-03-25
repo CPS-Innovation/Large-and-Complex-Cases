e2b08ebb9553a395223a97e20c827c174ddfcc32
Stef Stachow <stef@stachow.co.uk>
2025-03-25 21:21:37 +0000

    #OCB-38 - tidyups

--- e2e/pages/HomePage.ts
+++ e2e/pages/HomePage.ts
@@ -2,31 +2,17 @@ import { Locator, Page } from "@playwright/test";
 
 export class HomePage {
   static readonly route = "/";
-
   private readonly page: Page;
-  //readonly operationNameRadio: Locator;
   readonly operationName: Locator;
-  //readonly defendantNameRadio: Locator;
   readonly defendantName: Locator;
-  //  readonly urnRadio: Locator;
   readonly urn: Locator;
   readonly button: Locator;
 
   constructor(page: Page) {
     this.page = page;
-    // this.operationNameRadio = page
-    //   .getByTestId("radio-search-operation-name")
-    //   .first();
     this.operationName = page.getByTestId("search-operation-name").last();
-
-    // this.defendantNameRadio = page
-    //   .getByTestId("radio-search-defendant-name")
-    //   .first();
     this.defendantName = page.getByTestId("search-defendant-name");
-
-    //this.urnRadio = page.getByTestId("radio-search-urn").first();
     this.urn = page.getByTestId("search-urn");
-
     this.button = page.locator("button");
   }
 
