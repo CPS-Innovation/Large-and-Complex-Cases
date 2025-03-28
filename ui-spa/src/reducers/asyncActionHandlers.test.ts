611123f3865f231ebedb0707a72a5ff4c01e9c4a
Renjith Abby <renjithabby@gmail.com>
2025-03-28 10:45:49 +0000

    Story/LCC: 115 : Search error handling (#36)
    * minor refactor on the search result page
    
    * handled the api errors
    
    * minor urn bug fix
    
    * updated the unit and integration test
    
    * minor refactor

--- ui-spa/src/reducers/asyncActionHandlers.test.ts
+++ ui-spa/src/reducers/asyncActionHandlers.test.ts
@@ -7,8 +7,11 @@ vi.mock("../apis/gateway-api", () => {
 });
 
 describe("asyncActionHandlers", () => {
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
   describe("GET_CASE_DIVISIONS_OR_AREAS action", () => {
-    it("Should correctly dispatch actions", async () => {
+    it("Should correctly dispatch actions when getCaseDivisionsOrAreas is successful", async () => {
       const dispatchMock = vi.fn();
       const areaDivisions = { id: "1" };
       (getCaseDivisionsOrAreas as Mock).mockReturnValue(areaDivisions);
@@ -31,6 +34,33 @@ describe("asyncActionHandlers", () => {
       expect(dispatchMock).toHaveBeenNthCalledWith(2, {
         type: "UPDATE_CASE_DIVISIONS_OR_AREAS",
         payload: { status: "succeeded", data: areaDivisions },
+      });
+    });
+
+    it("Should correctly dispatch actions when getCaseDivisionsOrAreas failed", async () => {
+      const dispatchMock = vi.fn();
+      (getCaseDivisionsOrAreas as Mock).mockImplementation(() => {
+        throw new Error("errorMessage");
+      });
+      const handler = asyncActionHandlers.GET_CASE_DIVISIONS_OR_AREAS({
+        dispatch: dispatchMock,
+        getState: vi.fn(),
+        signal: new AbortController().signal,
+      });
+
+      await handler({
+        type: "GET_CASE_DIVISIONS_OR_AREAS",
+      });
+
+      expect(getCaseDivisionsOrAreas).toHaveBeenCalledTimes(1);
+      expect(dispatchMock).toHaveBeenCalledTimes(2);
+      expect(dispatchMock).toHaveBeenNthCalledWith(1, {
+        type: "UPDATE_CASE_DIVISIONS_OR_AREAS",
+        payload: { status: "loading" },
+      });
+      expect(dispatchMock).toHaveBeenNthCalledWith(2, {
+        type: "UPDATE_CASE_DIVISIONS_OR_AREAS",
+        payload: { status: "failed", error: "Error: errorMessage" },
       });
     });
   });
