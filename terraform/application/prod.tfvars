04dd2e47715f5b008569058724e0d44316467665
Mark Jones <aceltuk@users.noreply.github.com>
2025-04-02 09:44:00 +0100

    Bugfix/ocb28 pipeline and terraform fixes (#44)
    * Remove residual references to the staging slot that were missed
    
    * Add powershell installation to "deploy mock" task
    
    * Reconfigure API to be pinned to a single instance while durable orchestrations are part of the API's codebase
    
    * Try the latest deployment task-type
    
    * Change deployment method to be explicitly "run from package"
    
    * Tweaked storage account config and remove network service group references from application terraform.
    
    * Corrected syntax error in "Get Status URIs" when defining the resource group
    
    * Simplify mock deployment task
    
    * Add powershell installation task to Check Status Job
    
    * Add queue private endpoint for API to support orchestration calls
    
    * Commented out the mock check for now

--- terraform/application/prod.tfvars
+++ terraform/application/prod.tfvars
@@ -7,7 +7,6 @@ terraform_service_principal_display_name = "Azure Pipeline: LaCC-Prod"
 dns_server                               = "10.7.204.164"
 dns_alt_server                           = "168.63.129.16"
 subscription_id                          = "[Placeholder]"
-nsg_name                                 = "[Placeholder]"
 
 service_plans = {
   ui_service_plan_sku   = "P1mv3"
@@ -21,13 +20,13 @@ service_plans = {
 service_capacity = {
   ui_default_capacity   = 1
   ui_minimum_capacity   = 1
-  ui_max_capacity       = 10
+  ui_max_capacity       = 3
   api_default_capacity  = 1
   api_minimum_capacity  = 1
   api_max_capacity      = 1
   mock_default_capacity = 1
   mock_minimum_capacity = 1
-  mock_max_capacity     = 2
+  mock_max_capacity     = 3
 }
 
 subnets = {
--- terraform/application/storage-account-ui.tf
+++ terraform/application/storage-account-ui.tf
@@ -15,7 +15,7 @@ resource "azurerm_storage_account" "sacpsccui" {
   account_replication_type        = "RAGRS"
   account_tier                    = "Standard"
   min_tls_version                 = "TLS1_2"
-  public_network_access_enabled   = false
+  public_network_access_enabled   = true
   allow_nested_items_to_be_public = false
   shared_access_key_enabled       = true
 
@@ -24,7 +24,8 @@ resource "azurerm_storage_account" "sacpsccui" {
     bypass         = ["Metrics", "Logging", "AzureServices"]
     virtual_network_subnet_ids = [
       azurerm_subnet.sn_complex_cases_ui_subnet.id,
-      azurerm_subnet.sn_complex_cases_mock_subnet.id
+      azurerm_subnet.sn_complex_cases_endpoints_subnet.id,
+      data.azurerm_subnet.build_agent_subnet.id
     ]
   }
 
@@ -135,14 +136,6 @@ resource "azurerm_private_endpoint" "sacpsccui_file_pe" {
 resource "azapi_resource" "sacpsccui_file_share" {
   type      = "Microsoft.Storage/storageAccounts/fileServices/shares@2022-09-01"
   name      = "ui-content-share"
-  parent_id = "${data.azurerm_subscription.current.id}/resourceGroups/${azurerm_resource_group.rg_complex_cases.name}/providers/Microsoft.Storage/storageAccounts/${azurerm_storage_account.sacpsccui.name}/fileServices/default"
-
-  depends_on = [azurerm_storage_account.sacpsccui]
-}
-
-resource "azapi_resource" "sacpsccui_staging_file_share" {
-  type      = "Microsoft.Storage/storageAccounts/fileServices/shares@2022-09-01"
-  name      = "ui-content-share-1"
   parent_id = "${data.azurerm_subscription.current.id}/resourceGroups/${azurerm_resource_group.rg_complex_cases.name}/providers/Microsoft.Storage/storageAccounts/${azurerm_storage_account.sacpsccui.name}/fileServices/default"
 
   depends_on = [azurerm_storage_account.sacpsccui]
