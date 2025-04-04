0df636f810d5c3f63f16647212e05c2dcc6ee98f
Neil Foubister <neil.foubister@cps.gov.uk>
2025-04-04 10:45:45 +0100

    LCC-114: Initial commit. Endpoints decorated with OpenApi attributes and swagger page now available. (#45)

--- backend/CPS.ComplexCases.API/Functions/Status.cs
+++ backend/CPS.ComplexCases.API/Functions/Status.cs
@@ -1,7 +1,10 @@
+using System.Net;
 using System.Reflection;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using CPS.ComplexCases.API.Constants;
 using CPS.ComplexCases.API.Extensions;
 
 namespace CPS.ComplexCases.API.Functions;
@@ -9,6 +12,10 @@ namespace CPS.ComplexCases.API.Functions;
 public static class Status
 {
   [Function(nameof(Status))]
+  [OpenApiOperation(operationId: nameof(GetAreas), tags: ["Health"], Description = "Gets the current status of the function app.")]
+  [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: ContentType.ApplicationJson, bodyType: typeof(string), Description = ApiResponseDescriptions.Success)]
+  [OpenApiResponseWithBody(statusCode: HttpStatusCode.BadRequest, contentType: ContentType.TextPlain, typeof(string), Description = ApiResponseDescriptions.BadRequest)]
+  [OpenApiResponseWithBody(statusCode: HttpStatusCode.InternalServerError, contentType: ContentType.TextPlain, typeof(string), Description = ApiResponseDescriptions.InternalServerError)]
   public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "status")] HttpRequest req)
   {
     return Assembly.GetExecutingAssembly().CurrentStatus();
--- backend/CPS.ComplexCases.API/Functions/Tactical/CmsLoginDirect.cs
+++ backend/CPS.ComplexCases.API/Functions/Tactical/CmsLoginDirect.cs
@@ -1,9 +1,13 @@
+using System.Net;
 using System.Text.Json;
-using CPS.ComplexCases.API.Constants;
-using CPS.ComplexCases.DDEI.Tactical.Client;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.OpenApi.Models;
+using CPS.ComplexCases.API.Constants;
+using CPS.ComplexCases.DDEI.Tactical.Client;
+using CPS.ComplexCases.DDEI.Tactical.Models.Request;
 
 namespace CPS.ComplexCases.API.Functions.Tactical;
 
@@ -12,17 +16,30 @@ public class CmsLoginDirect(IDdeiClientTactical ddeiClient)
   private readonly IDdeiClientTactical _ddeiClient = ddeiClient;
 
   [Function(nameof(CmsLoginDirectGet))]
+  [OpenApiOperation(operationId: nameof(CmsLoginDirectGet), tags: ["CMS", "Authentication"], Description = "Returns the developer CMS login form.")]
+  [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: ContentType.TextPlain, bodyType: typeof(string), Description = ApiResponseDescriptions.Success)]
+  [OpenApiResponseWithBody(statusCode: HttpStatusCode.BadRequest, contentType: ContentType.TextPlain, typeof(string), Description = ApiResponseDescriptions.BadRequest)]
+  [OpenApiResponseWithBody(statusCode: HttpStatusCode.Unauthorized, contentType: ContentType.TextPlain, typeof(string), Description = ApiResponseDescriptions.Unauthorized)]
+  [OpenApiResponseWithBody(statusCode: HttpStatusCode.Forbidden, contentType: ContentType.TextPlain, typeof(string), Description = ApiResponseDescriptions.Forbidden)]
+  [OpenApiResponseWithBody(statusCode: HttpStatusCode.InternalServerError, contentType: ContentType.TextPlain, typeof(string), Description = ApiResponseDescriptions.InternalServerError)]
   public IActionResult CmsLoginDirectGet([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "tactical/login")] HttpRequest req) => new ContentResult
   {
     Content = HtmlHelpers.LoginForm(),
-    ContentType = "text/html"
+    ContentType = ContentType.TextHtml
   };
 
   [Function(nameof(CmsLoginDirectPost))]
+  [OpenApiOperation(operationId: nameof(CmsLoginDirectPost), tags: ["CMS", "Authentication"], Description = "Authenticates a user in CMS and returns a token and cookie.")]
+  [OpenApiRequestBody(contentType: ContentType.MultipartFormData, bodyType: typeof(AuthenticationRequest), Description = "Form data including username and password.")]
+  [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: ContentType.TextHtml, bodyType: typeof(string), Description = ApiResponseDescriptions.Success)]
+  [OpenApiResponseWithBody(statusCode: HttpStatusCode.BadRequest, contentType: ContentType.TextPlain, typeof(string), Description = ApiResponseDescriptions.BadRequest)]
+  [OpenApiResponseWithBody(statusCode: HttpStatusCode.Unauthorized, contentType: ContentType.TextPlain, typeof(string), Description = ApiResponseDescriptions.Unauthorized)]
+  [OpenApiResponseWithBody(statusCode: HttpStatusCode.Forbidden, contentType: ContentType.TextPlain, typeof(string), Description = ApiResponseDescriptions.Forbidden)]
+  [OpenApiResponseWithBody(statusCode: HttpStatusCode.InternalServerError, contentType: ContentType.TextPlain, typeof(string), Description = ApiResponseDescriptions.InternalServerError)]
   public async Task<IActionResult> CmsLoginDirectPost([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "tactical/login")] HttpRequest req)
   {
-    var username = req.Form["username"].ToString();
-    var password = req.Form["password"].ToString();
+    var username = req.Form[InputParameters.Username].ToString();
+    var password = req.Form[InputParameters.Password].ToString();
 
     string? content;
     try
@@ -41,7 +58,7 @@ public class CmsLoginDirect(IDdeiClientTactical ddeiClient)
     return new ContentResult
     {
       Content = content,
-      ContentType = "text/html"
+      ContentType = ContentType.TextHtml
     };
   }
 
