6861d80f81d6ef3ddbeb744ab3d4045838cd96d0
renjithabby <renjithabby@gmail.com>
2025-04-10 11:19:41 +0100

    minor refactor on the invalid date return

--- ui-spa/src/common/utils/formatDate.ts
+++ ui-spa/src/common/utils/formatDate.ts
@@ -1,13 +1,13 @@
 import { format, parseISO, isValid } from "date-fns";
 
-export const formatDate = (dateString: string) => {
-  if(!dateString) {
-    return null;
+export const formatDate = (dateString: string | null | undefined) => {
+  if (!dateString) {
+    return "--";
   }
   const date = parseISO(dateString);
 
   if (!isValid(date)) {
-    return "invalid date";
+    return "--";
   }
   return format(date, "dd/MM/yyyy");
 };
