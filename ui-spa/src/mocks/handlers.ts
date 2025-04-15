866747e1b6a6825f330c06d0245672caa7729564
renjithabby <renjithabby@gmail.com>
2025-04-15 10:31:22 +0100

    minor refactor

--- ui-spa/src/mocks/handlers.ts
+++ ui-spa/src/mocks/handlers.ts
@@ -16,7 +16,6 @@ export const setupHandlers = (baseUrl: string, apiMockSource: string) => {
       const results = isDevMock() ? caseAreasDev : caseAreasPlaywright;
       await delay(RESPONSE_DELAY);
       return HttpResponse.json(results);
-      // return new HttpResponse(null, { status: 500 });
     }),
 
     http.get(`${baseUrl}/api/case-search`, async () => {
@@ -25,7 +24,6 @@ export const setupHandlers = (baseUrl: string, apiMockSource: string) => {
         : casesSearchResultsPlaywright;
       await delay(RESPONSE_DELAY);
       return HttpResponse.json(caseSearchResults);
-      // return new HttpResponse(null, { status: 500 });
     }),
 
     http.get(`${baseUrl}/api/egress/workspaces`, async () => {
@@ -35,21 +33,10 @@ export const setupHandlers = (baseUrl: string, apiMockSource: string) => {
       await delay(RESPONSE_DELAY);
 
       return HttpResponse.json(egressSearchResults);
-      // return HttpResponse.json({
-      //   data: [],
-      //   pagination: {
-      //     totalResults: 50,
-      //     skip: 0,
-      //     take: 50,
-      //     count: 25,
-      //   },
-      // });
-      // return new HttpResponse(null, { status: 500 });
     }),
 
     http.post(`${baseUrl}/api/egress/connections`, async () => {
       return HttpResponse.json({});
-      // return new HttpResponse(null, { status: 500 });
     }),
   ];
 };
