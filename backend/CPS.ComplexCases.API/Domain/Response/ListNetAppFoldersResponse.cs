874adb152574b6f0fe8c3974aeea3caff65b0140
Neil Foubister <neil.foubister@cps.gov.uk>
2025-04-28 10:53:40 +0100

    LCC-152: Response data property updated to include folder, files and rootPath (#58)

--- backend/CPS.ComplexCases.API/Domain/Response/ListNetAppFoldersResponse.cs
+++ backend/CPS.ComplexCases.API/Domain/Response/ListNetAppFoldersResponse.cs
@@ -1,15 +1,32 @@
 namespace CPS.ComplexCases.API.Domain.Response;
 
-public class ListNetAppFoldersResponse
+public class ListNetAppObjectsResponse
 {
-    public required string BucketName { get; set; }
-    public string? RootPath { get; set; }
-    public required IEnumerable<ListNetAppFolderDataResponse> Data { get; set; }
+    public required ListNetAppObjectsDataResponse Data { get; set; }
     public required NetAppPaginationResponse Pagination { get; set; }
 }
 
-public class ListNetAppFolderDataResponse
+public class ListNetAppObjectsDataResponse
+{
+    public ListNetAppObjectsDataResponse()
+    {
+        Folders = [];
+        Files = [];
+    }
+
+    public string? RootPath { get; set; }
+    public required IEnumerable<ListNetAppFoldersDataResponse> Folders { get; set; } = [];
+    public required IEnumerable<ListNetAppFilesDataResponse> Files { get; set; } = [];
+}
+
+public class ListNetAppFoldersDataResponse
 {
     public required string FolderPath { get; set; }
     public int? CaseId { get; set; }
+}
+
+public class ListNetAppFilesDataResponse
+{
+    public required string FilePath { get; set; }
+    public required DateTime LastModifiedDate { get; set; }
 }
