ea54b6f4711b41c3556d93704f0c2a7eea832bf9
stef <stef@stachow.co.uk>
2025-05-12 09:08:49 +0100

    #OCB-65 - add AD username and correlation id to AppRequests (#35)
    * #OCB-65 - feature complete
    
    * #OCB-65 - add HttpTelemetry project as a dependency of the API project to ensure it is always built first
    
    ---------
    
    Co-authored-by: Rhys Bridges <rhys.bridges@cps.gov.uk>
    Co-authored-by: Neil Foubister <neil.foubister@cps.gov.uk>

---
+++ backend/CPS.ComplexCases.API.HttpTelemetry/Startup.cs
@@ -0,0 +1,39 @@
+﻿
+
+using CPS.ComplexCases.API.HttpTelemetry;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Description;
+using Microsoft.Azure.WebJobs.Host.Config;
+using Microsoft.Azure.WebJobs.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: WebJobsStartup(typeof(Startup))]
+
+namespace CPS.ComplexCases.API.HttpTelemetry;
+
+public class Startup : IWebJobsStartup2
+{
+    public void Configure(IWebJobsBuilder builder)
+    {
+        // wont be called
+    }
+
+    public void Configure(WebJobsBuilderContext context, IWebJobsBuilder builder)
+    {
+        builder.AddExtension<HttpTelemetryExtensionConfigProvider>();
+        builder.Services.AddSingleton<ITelemetryInitializer, HttpTelemetryInitializer>();
+        builder.Services.AddHttpContextAccessor();
+    }
+}
+
+[Extension("HttpTelemetry")]
+internal class HttpTelemetryExtensionConfigProvider : IExtensionConfigProvider
+{
+    public void Initialize(ExtensionConfigContext context)
+    {
+        var bindingRule = context.AddBindingRule<HttpTelemetryAttribute>();
+        bindingRule.BindToInput<string?>(attr => "");
+    }
+}
+
---
+++ backend/CPS.ComplexCases.API/Attributes/HttpTelemetryAttribute.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Azure.Functions.Worker.Extensions.Abstractions;
+
+[assembly: ExtensionInformation("CPS.ComplexCases.API.HttpTelemetry", "1.0.41")]
+
+namespace CPS.ComplexCases.API.Attributes
+{
+    [AttributeUsage(AttributeTargets.Parameter)]
+    public sealed class HttpTelemetryAttribute : InputBindingAttribute
+    {
+    }
+}
