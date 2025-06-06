12bf89e258ae302a60acddfb130b929d2c277b01
unknown <rhys.bridges@cps.gov.uk>
2025-06-06 11:04:32 +0100

    FCT2_6521 check if a transfer is in progress for the caseid

--- backend/CPS.ComplexCases.FileTransfer.API/Functions/InitiateTransfer.cs
+++ backend/CPS.ComplexCases.FileTransfer.API/Functions/InitiateTransfer.cs
@@ -13,6 +13,9 @@ using CPS.ComplexCases.FileTransfer.API.Durable.Payloads;
 using CPS.ComplexCases.FileTransfer.API.Models.Responses;
 using CPS.ComplexCases.FileTransfer.API.Models.Domain.Enums;
 using CPS.ComplexCases.Common.Services;
+using Microsoft.DurableTask.Entities;
+using CPS.ComplexCases.FileTransfer.API.Durable.State;
+using CPS.ComplexCases.FileTransfer.API.Durable.Payloads.Domain;
 
 namespace CPS.ComplexCases.FileTransfer.API.Functions;
 
@@ -37,6 +40,25 @@ public class InitiateTransfer
         if (!transferRequest.IsValid)
         {
             return new BadRequestObjectResult(transferRequest.ValidationErrors);
+        }
+
+        var caseMetadata = await _caseMetadataService.GetCaseMetadataForCaseIdAsync(transferRequest.Value.Metadata.CaseId);
+
+        if (caseMetadata?.ActiveTransferId.HasValue == true)
+        {
+            var entityId = new EntityInstanceId(nameof(TransferEntityState), caseMetadata.ActiveTransferId.Value.ToString());
+            var entityState = await orchestrationClient.Entities.GetEntityAsync<TransferEntity>(entityId);
+
+            if (entityState != null && entityState.State != null && entityState.State.Status != TransferStatus.Completed)
+            {
+                // if the transfer is already in progress, return the current status
+                return new AcceptedResult($"/api/filetransfer/{caseMetadata.ActiveTransferId}/status", new TransferResponse
+                {
+                    Id = caseMetadata.ActiveTransferId.Value,
+                    Status = entityState.State.Status,
+                    CreatedAt = entityState.State.CreatedAt,
+                });
+            }
         }
 
         var currentCorrelationId = req.Headers.GetCorrelationId();
