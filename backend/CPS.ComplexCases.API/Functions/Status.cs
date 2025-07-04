44fc72d9c252c03d9cd121fe951177737e8a2a31
fcteas\Hasan.Ahmed <Hasan.Ahmed@cps.gov.uk>
2025-07-04 17:17:24 +0000

    PR review suggested changes..

--- backend/CPS.ComplexCases.API/Functions/Status.cs
+++ backend/CPS.ComplexCases.API/Functions/Status.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Reflection;
+using CPS.ComplexCases.API.Attributes;
 using CPS.ComplexCases.Common.Constants;
 using CPS.ComplexCases.Common.Functions;
 using CPS.ComplexCases.Common.Models.Domain;
@@ -17,7 +18,7 @@ public static class Status
   [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: ContentType.ApplicationJson, bodyType: typeof(AssemblyStatus), Description = ApiResponseDescriptions.Success)]
   [OpenApiResponseWithBody(statusCode: HttpStatusCode.BadRequest, contentType: ContentType.TextPlain, typeof(string), Description = ApiResponseDescriptions.BadRequest)]
   [OpenApiResponseWithBody(statusCode: HttpStatusCode.InternalServerError, contentType: ContentType.TextPlain, typeof(string), Description = ApiResponseDescriptions.InternalServerError)]
-  public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "status")] HttpRequest req)
+  public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "status")] HttpRequest req, [HttpTelemetry] object leaveThisInPlace)
   {
     return StatusFunction.GetStatus(Assembly.GetExecutingAssembly());
   }
