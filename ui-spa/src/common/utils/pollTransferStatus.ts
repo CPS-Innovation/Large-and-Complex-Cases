d3b315a68802a88b99730890deba53788e318be9
renjithabby <renjithabby@gmail.com>
2025-07-09 10:17:25 +0100

    added comment for the function as per the comment review

--- ui-spa/src/common/utils/pollTransferStatus.ts
+++ ui-spa/src/common/utils/pollTransferStatus.ts
@@ -2,6 +2,14 @@ import { getTransferStatus } from "../../apis/gateway-api";
 import { TransferStatusResponse } from "../types/TransferStatusResponse";
 import { ApiError } from "../../common/errors/ApiError";
 
+/**
+ * Polls the transfer status endpoint at regular intervals until a terminal state is reached or polling should stop
+ * @param transferId - The ID of the transfer to check status for
+ * @param shouldStopPolling - Function that determines if polling should be stopped
+ * @param handleResponse - Callback function to process each status response
+ * @param handleError - Callback function to handle any errors during polling
+ * @param pollingInterval - Time in milliseconds between poll attempts (default: 1000ms)
+ */
 export const pollTransferStatus = async (
   transferId: string,
   shouldStopPolling: () => boolean,
