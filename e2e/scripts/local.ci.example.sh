453dbaaa54d8f1b7bf46fb317f926456809c2f8b
Lilach.Davis <Lilach.Davis@cps.gov.uk>
2025-08-26 11:20:30 +0100

    add deployment to staging

--- e2e/scripts/local.ci.example.sh
+++
@@ -1,4 +0,0 @@
-ENVIRONMENT=local \
-E2E_AD_USER=The automation AD user email goes here  \
-E2E_PASSWORD=The automation AD user password goes here \
-npm run e2e:ci
--- e2e/tests/auth.setup.ts
+++
@@ -1,9 +0,0 @@
-import { test as setup } from "@playwright/test";
-import { LoginPage } from "../pages/LoginPage";
-
-setup("authenticate", async ({ page }) => {
-  const login = new LoginPage(page);
-  await login.loginToCms();
-  await login.loginToAd();
-  await login.persistLoginInfo();
-});
