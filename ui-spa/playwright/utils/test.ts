16eb81b149b11ddbf100a86eaa0d7b39aa5ab775
Lilach <Lilach.Davis@cps.gov.uk>
2025-10-21 15:29:56 +0100

    Fct2 8617/add e2e coverage report (#176)

--- ui-spa/playwright/utils/test.ts
+++ ui-spa/playwright/utils/test.ts
@@ -2,6 +2,10 @@ import { test as base, expect } from "@playwright/test";
 import { http } from "msw";
 import type { MockServiceWorker } from "playwright-msw";
 import { createWorkerFixture } from "playwright-msw";
+import type { CoverageMapData } from "istanbul-lib-coverage";
+import fs from "fs";
+import path from "path";
+import { randomUUID } from "crypto";
 
 import { setupHandlers } from "../../src/mocks/handlers";
 
@@ -13,6 +17,19 @@ const test = base.extend<{
     setupHandlers("https://mocked-out-api", "playwright"),
   ),
   http,
+});
+
+const nycOutputDir = path.join(process.cwd(), "playwright", ".nyc_output")
+
+test.afterEach(async ({ page }) => {
+  const cov = await page.evaluate(() => (globalThis as unknown as CoverageMapData | undefined)?.__coverage__);
+  if (cov) {
+    fs.mkdirSync(nycOutputDir, { recursive: true });
+    fs.writeFileSync(
+      path.join(nycOutputDir, `pw-${randomUUID()}.json`),
+      JSON.stringify(cov),
+    );
+  }
 });
 
 export { expect, test };
