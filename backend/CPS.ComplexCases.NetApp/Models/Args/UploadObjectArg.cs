219688322d2298448df401e2af2cbfd03f940373
Neil Foubister <neil.foubister@cps.gov.uk>
2026-01-14 09:24:54 +0000

    FCT2-13508: Fix to how DisablePayloadSigning is set in NetAppArgFactory

--- backend/CPS.ComplexCases.NetApp/Models/Args/UploadObjectArg.cs
+++ backend/CPS.ComplexCases.NetApp/Models/Args/UploadObjectArg.cs
@@ -5,4 +5,5 @@ public class UploadObjectArg : BaseNetAppArg
     public required string ObjectKey { get; set; }
     public required Stream Stream { get; set; }
     public required long ContentLength { get; set; }
+    public bool DisablePayloadSigning { get; set; } = true;
 }
