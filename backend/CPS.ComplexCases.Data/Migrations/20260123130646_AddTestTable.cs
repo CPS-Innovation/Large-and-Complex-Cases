59aeec1d72abc34779a29448bd9c8f3162843ba6
Lilach.Davis <Lilach.Davis@cps.gov.uk>
2026-01-23 13:37:46 +0000

    test rollback

---
+++ backend/CPS.ComplexCases.Data/Migrations/20260123130646_AddTestTable.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace CPS.ComplexCases.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTestTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "test_table",
+                schema: "large_complex_cases",
+                columns: table => new
+                {
+                    test_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    test_column = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_test_table", x => x.test_id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "test_table",
+                schema: "large_complex_cases");
+        }
+    }
+}
