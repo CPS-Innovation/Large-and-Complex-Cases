cee1067b66f6b86a2a130e166934f09e28740cee
Lilach <Lilach.Davis@cps.gov.uk>
2026-04-14 12:47:57 +0100

    add test file cleanup

--- e2e/postman/Setup-EgressWorkspaceAndUpload.ps1
+++ e2e/postman/Setup-EgressWorkspaceAndUpload.ps1
@@ -796,6 +796,8 @@ Write-Host "##vso[task.setvariable variable=isTestWorkspaceCreated]$isTestWorksp
 Write-Host "##vso[task.setvariable variable=egressWorkspaceId]$WorkspaceId"
 Write-Host "##vso[task.setvariable variable=egressWorkspaceName]$WorkspaceName"
 if (-not $SkipUpload -and $UploadedFiles.Count -gt 0) {
+    Write-Host "##vso[task.setvariable variable=isFilesUploaded]true"
+
     $firstFile = $UploadedFiles[0]
     Write-Host "##vso[task.setvariable variable=egressFileId]$($firstFile.FileId)"
     Write-Host "##vso[task.setvariable variable=egressUploadId]$($firstFile.UploadId)"
