8e721a6f9942dd65c16e8b646f1f56f8bbc04d18
renjithabby <renjithabby@gmail.com>
2026-05-11 12:34:05 +0100

    updated the response schema validation for the search results

--- ui-spa/src/schemas/responses/searchResult.ts
+++ ui-spa/src/schemas/responses/searchResult.ts
@@ -1,14 +1,23 @@
 import { z } from "zod";
 
-export const searchResultSchema = z.object({
-  operationName: z.string(),
-  urn: z.string(),
-  caseId: z.number(),
-  leadDefendantName: z.string(),
-  egressWorkspaceId: z.string().nullable(),
-  netappFolderPath: z.string().nullable(),
-  registrationDate: z.string().nullable(),
-});
+export const searchResultSchema = z
+  .object({
+    operationName: z.string().nullable(),
+    urn: z.string(),
+    caseId: z.number(),
+    leadDefendantName: z.string().nullable(),
+    egressWorkspaceId: z.string().nullable(),
+    netappFolderPath: z.string().nullable(),
+    registrationDate: z.string().nullable(),
+  })
+  .superRefine((val, ctx) => {
+    // both operationName and leadDefendantName cannot be null at the same time
+    if (val.operationName === null && val.leadDefendantName === null) {
+      ctx.addIssue(
+        "Both `operationName` and `leadDefendantName` cannot both be null",
+      );
+    }
+  });
 
 export const searchResultDataSchema = z.array(searchResultSchema);
 
