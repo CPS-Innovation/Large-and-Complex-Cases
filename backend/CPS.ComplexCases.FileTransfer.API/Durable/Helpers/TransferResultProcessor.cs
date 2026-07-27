5efac88242a445077ec3a076f13770d2eb7f9c22
unknown <rhys.bridges@cps.gov.uk>
2026-07-27 11:10:00 +0100

    small pr comment fixes

--- backend/CPS.ComplexCases.FileTransfer.API/Durable/Helpers/TransferResultProcessor.cs
+++ backend/CPS.ComplexCases.FileTransfer.API/Durable/Helpers/TransferResultProcessor.cs
@@ -1,8 +1,10 @@
 using CPS.ComplexCases.FileTransfer.API.Durable.Payloads.Domain;
 using CPS.ComplexCases.FileTransfer.API.Durable.State;
+using CPS.ComplexCases.FileTransfer.API.Models.Domain.Enums;
 using CPS.ComplexCases.FileTransfer.API.Telemetry;
 using Microsoft.DurableTask;
 using Microsoft.DurableTask.Entities;
+using Microsoft.Extensions.Logging;
 
 namespace CPS.ComplexCases.FileTransfer.API.Durable.Helpers;
 
@@ -16,16 +18,23 @@ public static class TransferResultProcessor
         TransferOrchestrationEvent telemetryEvent,
         bool isRetry = false)
     {
+        var logger = context.CreateReplaySafeLogger(nameof(TransferResultProcessor));
+
         foreach (var result in results)
         {
-            if (result != null && result.IsSkipped && result.SkippedItem != null)
+            if (result is null)
+            {
+                continue;
+            }
+
+            if (result.IsSkipped && result.SkippedItem != null)
             {
                 await context.Entities.CallEntityAsync(
                     entityId,
                     nameof(TransferEntityState.AddSkippedItem),
                     result.SkippedItem);
             }
-            else if (result != null && result.IsSuccess && result.SuccessfulItem != null)
+            else if (result.IsSuccess && result.SuccessfulItem != null)
             {
                 await context.Entities.CallEntityAsync(
                     entityId,
@@ -36,13 +45,41 @@ public static class TransferResultProcessor
                 telemetryEvent.TotalFilesTransferred++;
                 telemetryEvent.TotalBytesTransferred += result.SuccessfulItem.Size;
             }
-            else if (result != null && !result.IsSuccess && result.FailedItem != null)
+            else if (!result.IsSuccess && result.FailedItem != null)
             {
                 await context.Entities.CallEntityAsync(
                     entityId,
                     isRetry ? nameof(TransferEntityState.AddFailedRetryItem)
                             : nameof(TransferEntityState.AddFailedItem),
                     result.FailedItem);
+
+                telemetryEvent.TotalFilesFailed++;
+            }
+            else
+            {
+                logger.LogWarning(
+                    "Unclassifiable transfer result. IsSuccess={IsSuccess}, IsSkipped={IsSkipped}, HasSuccessfulItem={HasSuccessfulItem}, HasSkippedItem={HasSkippedItem}, HasFailedItem={HasFailedItem}",
+                    result.IsSuccess,
+                    result.IsSkipped,
+                    result.SuccessfulItem is not null,
+                    result.SkippedItem is not null,
+                    result.FailedItem is not null);
+
+                var failedItem = result.FailedItem ?? new TransferFailedItem
+                {
+                    SourcePath = result.SuccessfulItem?.SourcePath
+                        ?? result.SkippedItem?.SourcePath
+                        ?? "unknown",
+                    Status = TransferItemStatus.Failed,
+                    ErrorCode = TransferErrorCode.GeneralError,
+                    ErrorMessage = "Unclassifiable transfer result"
+                };
+
+                await context.Entities.CallEntityAsync(
+                    entityId,
+                    isRetry ? nameof(TransferEntityState.AddFailedRetryItem)
+                            : nameof(TransferEntityState.AddFailedItem),
+                    failedItem);
 
                 telemetryEvent.TotalFilesFailed++;
             }
