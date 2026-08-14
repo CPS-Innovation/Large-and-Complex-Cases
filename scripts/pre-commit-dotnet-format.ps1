c71add5e83aa848ca0e085eb1de73e1c599dea45
Lilach <Lilach.Davis@cps.gov.uk>
2026-08-14 11:35:24 +0100

    update dotnet format pre-commit hook script

---
+++ scripts/pre-commit-dotnet-format.ps1
@@ -0,0 +1,19 @@
+param(
+    [Parameter(ValueFromRemainingArguments = $true)]
+    [string[]]$Files
+)
+
+if (-not $Files -or $Files.Count -eq 0) {
+    exit 0
+}
+
+$solution="backend/CPS.ComplexCases.sln"
+
+dotnet format $solution `
+    --no-restore `
+    --include `
+    $Files
+
+git diff --name-only -- $Files
+
+exit $LASTEXITCODE
