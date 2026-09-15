7ab2660b49e7955ed877a22a9e52cc8e9f122d13
unknown <rhys.bridges@cps.gov.uk>
2026-09-15 09:20:03 +0100

    formatting

--- backend/CPS.ComplexCases.FileTransfer.API/Durable/Activity/UpdateActivityLog.cs
+++ backend/CPS.ComplexCases.FileTransfer.API/Durable/Activity/UpdateActivityLog.cs
@@ -9,6 +9,9 @@ using CPS.ComplexCases.FileTransfer.API.Durable.Helpers;
 using CPS.ComplexCases.FileTransfer.API.Durable.Payloads;
 using CPS.ComplexCases.FileTransfer.API.Durable.Payloads.Domain;
 using CPS.ComplexCases.FileTransfer.API.Durable.State;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.DurableTask.Client;
+using Microsoft.DurableTask.Entities;
 using Microsoft.Extensions.Logging;
 
 namespace CPS.ComplexCases.FileTransfer.API.Durable.Activity;
